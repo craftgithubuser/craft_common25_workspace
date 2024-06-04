c69a690d86781c46022e2d955531c197fbbbfd4c
Tewodros-Adane <tedyyyyy1047@gmail.com>
2024-06-03 22:17:48 -0400

    commit from eclipse

--- Training/src/pack/ins/example1.java
+++ Training/src/pack/ins/example1.java
@@ -3,8 +3,7 @@ package pack.ins;
 public class example1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.print("hello craft");
+     System.out.print("I wanna connect eclipse to git");
      System.out.print("I wanna connect eclipse to git");
     
 	}
